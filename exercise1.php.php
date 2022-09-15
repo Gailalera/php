ee5cd0355c260eada0be3938d8b22fd4f8f30f60
CIT173F-3@M307.UC-BCF.EDU.PH <CIT173F-3@M307.UC-BCF.EDU.PH>
2022-09-15 11:31:25 +0800

    upload

---
+++ exercise1.php.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Exercise #1</title>
+</head>
+<body>
+	<h1>Twinkle Twinkle little star</h1>
+	<?php
+	echo "Twinkle Twinkle little star.<br>";
+    $name = "Twinkle";
+    $another_name = "star";
+    	print "$name $name little $another_name";
+	  ?>
+
+</body>
+</html>	
---
+++ exercise2.php.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>exercise #2</title>
+</head>
+<body>
+	<?php
+		$x = 10;
+		$y = 7;
+
+		$result = $x+$y;
+		echo "$x + $y = $result <br />";
+
+		$result=$x-$y; 
+		echo "$x - $y = $result<br />";   
+ 
+		$result=$x*$y; 
+		echo "$x * $y = $result<br />";   
+		 
+		$result=$x/$y; 
+		echo "$x / $y = $result<br />";   
+		 
+		$result=$x%$y; 
+		echo "$x % $y = $result<br />";   
+
+
+	?>
+
+</body>
+</html>
